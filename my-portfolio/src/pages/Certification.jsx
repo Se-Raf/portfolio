da34d2cfe022a785450d9f892ddad5665fa76c5b
Rafael Senados <senados.rafael@gmail.com>
2026-01-20 14:57:54 +0800

    Pages and routing

---
+++ my-portfolio/src/pages/Certification.jsx
@@ -0,0 +1,9 @@
+const Certification = () => {
+    return (
+        <div>
+            <h1>Certification</h1>
+        </div>
+    );
+};
+
+export default Certification;
---
+++ my-portfolio/src/pages/TechStack.jsx
@@ -0,0 +1,9 @@
+const TechStack = () => {
+    return (
+        <div>
+            <h1>Tech Stack</h1>
+        </div>
+    );
+};
+
+export default TechStack;
