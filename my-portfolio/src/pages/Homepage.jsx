d3b338ee0ea0f20c8bc39d728b7111d1c438ece4
Rafael Senados <senados.rafael@gmail.com>
2026-01-22 22:46:34 +0800

    Home page layout 20%

--- my-portfolio/src/pages/Homepage.jsx
+++ my-portfolio/src/pages/Homepage.jsx
@@ -1,11 +1,16 @@
 import Navbar from '../components/Navbar';
 import Menu from '../components/Menu';
+import TechStackMenu from '../components/TechStackMenu';
+import "./Homepage.css"
 
 const Homepage = () => {
     return (
         <div>
             <Navbar />
-            <h1>Home</h1>
+            <div className="homepage-banner">
+                <h1>Home</h1>
+            </div>
+            <TechStackMenu />
             <Menu />
         </div>
     );
